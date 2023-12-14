2cb12341f1212d73833614876c7020867bb62a96
Dejurniy <100092180+Dejurniy@users.noreply.github.com>
2023-12-14 16:34:26 +0400

    Delete Desktop directory

--- Desktop/Rest_Project/Proj/User/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from User.views import user_views
-
-urlpatterns = [
-    path("", user_views.UserListCreateView.as_view()),
-    path("<int:pk>/", user_views.UserRetrieveUpdateView.as_view()),
-]
--- Desktop/dockerizedAPI/carAPI/cars/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Cars(models.Model):
-    id = models.AutoField(unique=True, primary_key=True)
-    mark = models.CharField(max_length=20)
-    model = models.CharField(max_length=20, unique=True)
